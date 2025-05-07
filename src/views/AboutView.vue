d21b27860b958756140bad964d6fe4a2fab82a4d
Jon Wynveen <jonwynveen@gmail.com>
2025-05-07 12:06:06 +0300

    Add wynsome creations attribution

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,16 +1,42 @@
+<script setup>
+import Icon from '../components/Icon.vue'
+</script>
 <template>
   <div class="about">
     <h1>ePub Press</h1>
 
-    <p>ePub Press is your ePub Printing Press. Easily edit any .epub file as needed and download the updated version.</p>
+    <p>
+      ePub Press is your ePub Printing Press. Easily edit any .epub file as needed and download the
+      updated version.
+    </p>
 
-    <sub>Logo created from: offset printing by Vectors Point from <a href="https://thenounproject.com/browse/icons/term/offset-printing/" target="_blank" title="offset printing Icons">Noun Project</a> (CC BY 3.0) </sub>
+    <p>
+      A product of:
+      <Icon name="wynsome-creations" class="logo" />
+    </p>
+    <sub
+      >Logo created from: offset printing by Vectors Point from
+      <a
+        href="https://thenounproject.com/browse/icons/term/offset-printing/"
+        target="_blank"
+        title="offset printing Icons"
+        >Noun Project</a
+      >
+      (CC BY 3.0)
+    </sub>
   </div>
 </template>
 
-<style>
+<style scoped>
 .about {
   padding: 1rem;
+}
+
+.logo {
+  height: 3rem;
+  width: auto;
+  display: block;
+  margin: 1rem 0;
 }
 @media (min-width: 1024px) {
   .about {
